31dc6d9c034386600523aae1230b826e2860d27a
chenzehua <815054699@qq.com>
2017-10-24 16:25:50 +0800

    '吉粮mobile注册页面ios输入验证码选择带空格正则不通过修复，onresize支持多个添加'

--- js/init.js
+++ js/init.js
@@ -1,5 +1,41 @@
-;
-(function(doc, win) {
+;(function(doc, win) {
+	/*
+	 window.onresize 兼容多个
+	 * */
+	var resize_arr = [],
+		resize_naming = 0;
+	window.addResize = function (fun) {
+		if(!fun || fun && !(fun instanceof Function) ){return false;}
+		var name  = fun.name ? fun.name : 'undefined_'+resizeNaming();
+		resize_arr.push({
+			"name":name,
+			"callback":fun
+		});
+		integrationCallback();
+	}
+	function resizeNaming() {
+		return ++resize_naming;
+	}
+	function integrationCallback() {
+		var arr = resize_arr,
+			len = arr.length,
+			f = function(){};
+		if(!arr || arr && !(arr instanceof Array)){return false;}
+		window.onresize = undefined;
+		for(var i = 0 ; i<len;i++){
+			console.log(i);
+			f = window.onresize;
+			window.onresize =  f ?
+				function(e){
+					f(e);
+					arr[i].callback(e);
+				}:arr[i].callback;
+		};
+		i--;
+	}
+	
+	
+	
 	window.onLoad = function(fun) {
 		var defaults = window.onload;
 		window.onload = defaults ?
@@ -19,7 +55,7 @@
 		doc.documentElement.style.fontSize = 100 * (clientW >= maxW ? 1 : clientW / maxW) + 'px';
 	};
 	defineRem();
-	window.onresize = function(e){
-		defineRem();
-	}
+//	window.onresize = function(e){
+//		defineRem();
+//	};
 }(document, window));
