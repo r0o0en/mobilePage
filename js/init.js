df443c759b3465d00c26e6e1c2596c4336caf007
chenzehua <815054699@qq.com>
2017-11-01 10:13:04 +0800

    主题活动-1静态页完成，附带一个简单的jquery.form.js

--- js/init.js
+++ js/init.js
@@ -1,41 +1,4 @@
 ;(function(doc, win) {
-	/*
-	 window.onresize 兼容多个
-	 * */
-	var resize_arr = [],
-		resize_naming = 0;
-	window.addResize = function (fun) {
-		if(!fun || fun && !(fun instanceof Function) ){return false;}
-		var name  = fun.name ? fun.name : 'undefined_'+resizeNaming();
-		resize_arr.push({
-			"name":name,
-			"callback":fun
-		});
-		integrationCallback();
-	}
-	function resizeNaming() {
-		return ++resize_naming;
-	}
-	function integrationCallback() {
-		var arr = resize_arr,
-			len = arr.length,
-			f = function(){};
-		if(!arr || arr && !(arr instanceof Array)){return false;}
-		window.onresize = undefined;
-		for(var i = 0 ; i<len;i++){
-			console.log(i);
-			f = window.onresize;
-			window.onresize =  f ?
-				function(e){
-					f(e);
-					arr[i].callback(e);
-				}:arr[i].callback;
-		};
-		i--;
-	}
-	
-	
-	
 	window.onLoad = function(fun) {
 		var defaults = window.onload;
 		window.onload = defaults ?
@@ -45,7 +8,6 @@
 			} :
 			fun;
 	};
-
 	function defineRem(maxW) {
 		/*
 		 clientW >= maxW 时  1rem = 100px , 小于等于时 1rem = 100 * clientW/maxW;
@@ -55,7 +17,12 @@
 		doc.documentElement.style.fontSize = 100 * (clientW >= maxW ? 1 : clientW / maxW) + 'px';
 	};
 	defineRem();
-//	window.onresize = function(e){
-//		defineRem();
-//	};
+	window.onresize = function(e){
+		defineRem();
+	};
+	
+	//如果是测试连接，显示刷新按钮
+	if(location.host == '192.168.1.188'&&document.getElementById('flexRefreshBtn')){
+		document.getElementById('flexRefreshBtn').style.display = 'block';
+	}
 }(document, window));
