25ed20552c48aec6b45964209c8d556bfdb114bf
chenzehua <815054699@qq.com>
2017-11-09 16:13:24 +0800

    双11详情页 elevent js判断ios添加平滑滚动

--- public/js/init.js
+++ public/js/init.js
@@ -1,4 +1,6 @@
-;(function(doc, win) {
+;
+(function(doc, win) {
+
 	window.onLoad = function(fun) {
 		var defaults = window.onload;
 		window.onload = defaults ?
@@ -8,6 +10,19 @@
 			} :
 			fun;
 	};
+	window.onResize = function(fun) {
+		if(!fun || fun && !(fun instanceof Function)) {
+			return false;
+		}
+		var defaults = window.onresize;
+		window.onresize = defaults ?
+			function(e) {
+				defaults(e);
+				fun(e);
+			} :
+			fun;
+	}
+
 	function defineRem(maxW) {
 		/*
 		 clientW >= maxW 时  1rem = 100px , 小于等于时 1rem = 100 * clientW/maxW;
@@ -16,13 +31,70 @@
 			clientW = doc.documentElement.clientWidth ? doc.documentElement.clientWidth : doc.body.clientWidth;
 		doc.documentElement.style.fontSize = 100 * (clientW >= maxW ? 1 : clientW / maxW) + 'px';
 	};
-	defineRem();
-	window.onresize = function(e){
-		defineRem();
+
+	//获取参数
+	window.getRequest = function() {
+		var href = location.search;
+		var url = decodeURI(href); //获取url中"?"符后的字串
+		var theRequest = new Object();
+		if(url.indexOf("?") != -1) {
+			var str = url.substr(1);
+			strs = str.split("&");
+			for(var i = 0; i < strs.length; i++) {
+				theRequest[strs[i].split("=")[0]] = strs[i].split("=")[1];
+			}
+		}
+		return theRequest;
+	}
+	//检测浏览器
+	window.browser = {
+		versions: function() {
+			var u = navigator.userAgent,
+				app = navigator.appVersion;
+			return {
+				trident: u.indexOf('Trident') > -1, //IE内核
+				presto: u.indexOf('Presto') > -1, //opera内核
+				webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+				gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+				mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+				ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+				android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
+				iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+				iPad: u.indexOf('iPad') > -1, //是否iPad
+				webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+				weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
+				qq: u.match(/\sQQ/i) == " qq" //是否QQ
+			};
+		}(),
+		language: (navigator.browserLanguage || navigator.language).toLowerCase()
 	};
-	
+	//给body 添加 平滑滚动
+	window.smoothRoll = function() {
+		if(browser.versions.ios || browser.versions.iPad) {
+//			onLoad(function() {
+				var body = document.body,
+					html = document.getElementsByTagName('html')[0],
+					box = document.querySelector('body>*');
+				var classs = box.getAttribute('class'),
+					bodystyle = body.getAttribute('style'),
+					htmlstyle = html.getAttribute('style'),
+					boxstyle = box.getAttribute('style');
+				html.setAttribute('style', htmlstyle + 'background-color: white;');
+				body.setAttribute('style', bodystyle + 'height: 100%;overflow: hidden;');
+				box.setAttribute('style', htmlstyle + 'height: 100%;overflow: auto;    position: relative;z-index: 1;');
+				box.setAttribute('class', classs + ' smooth-roll');
+//			});
+		}
+	}
+	/*
+	 init
+	 * */
+	defineRem();
+	onResize(function(e) {
+		defineRem();
+	});
 	//如果是测试连接，显示刷新按钮
-	if(location.host == '192.168.1.188'&&document.getElementById('flexRefreshBtn')){
+	if(location.host == '192.168.1.188' && document.getElementById('flexRefreshBtn')) {
 		document.getElementById('flexRefreshBtn').style.display = 'block';
 	}
 }(document, window));
